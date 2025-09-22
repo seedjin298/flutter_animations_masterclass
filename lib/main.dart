e1c56b4e6a62979673ab37badd538e273f89933c
seedjin298 <seedmemoria@gmail.com>
2025-09-22 12:34:25 +0900

    #1.0 Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations_masterclass/screens/menu_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Animations Masterclass',
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Colors.blue,
+        ),
+      ),
+      home: MenuScreen(),
+    );
+  }
+}
