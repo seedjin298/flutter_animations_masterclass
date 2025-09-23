0fabcc53b8ae0314842ab008c20b0007e1240f69
seedjin298 <seedmemoria@gmail.com>
2025-09-24 01:09:22 +0900

    #2.8 AnimationStatus

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -47,7 +47,7 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
   late final CurvedAnimation _curve = CurvedAnimation(
     parent: _animationController,
     curve: Curves.elasticOut,
-    reverseCurve: Curves.bounceIn,
+    reverseCurve: Curves.bounceOut,
   );
 
   void _play() {
@@ -67,6 +67,19 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
   void _onChanged(double value) {
     _range.value = 0;
     _animationController.value = value;
+  }
+
+  bool _looping = false;
+
+  void _toggleLooping() {
+    if (_looping) {
+      _animationController.stop();
+    } else {
+      _animationController.repeat(reverse: true);
+    }
+    setState(() {
+      _looping = !_looping;
+    });
   }
 
   @override
@@ -119,7 +132,13 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
                 ElevatedButton(
                   onPressed: _rewind,
                   child: const Text("Rewind"),
-                )
+                ),
+                ElevatedButton(
+                  onPressed: _toggleLooping,
+                  child: Text(
+                    _looping ? "Stop looping" : "Start looping",
+                  ),
+                ),
               ],
             ),
             SizedBox(
