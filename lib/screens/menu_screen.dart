6e9ade23348478399d7d3c43e9f0b3251750aaa0
seedjin298 <seedmemoria@gmail.com>
2025-09-23 15:39:27 +0900

    #2.0 Implicit vs Explicit

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_masterclass/screens/explicit_animations_screen.dart';
 import 'package:flutter_animations_masterclass/screens/implicit_animations_screen.dart';
 
 class MenuScreen extends StatelessWidget {
@@ -30,6 +31,15 @@ class MenuScreen extends StatelessWidget {
                 );
               },
               child: Text("Implicit Animations"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  ExplicitAnimationsScreen(),
+                );
+              },
+              child: Text("Explicit Animations"),
             ),
           ],
         ),
