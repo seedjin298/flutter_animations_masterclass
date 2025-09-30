48f5d638c6a7d317733482aa23eaf8010e491c02
seedjin298 <seedmemoria@gmail.com>
2025-09-30 21:54:26 +0900

    #5.3 Hero

---
+++ lib/screens/music_player_detail_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayerDetailScreen extends StatefulWidget {
+  final int index;
+
+  const MusicPlayerDetailScreen({
+    super.key,
+    required this.index,
+  });
+
+  @override
+  State<MusicPlayerDetailScreen> createState() =>
+      _MusicPlayerDetailScreenState();
+}
+
+class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Interstellar"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Hero(
+              tag: "${widget.index}",
+              child: Container(
+                height: 350,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.4),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: Offset(0, 8),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/covers/${widget.index}.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
